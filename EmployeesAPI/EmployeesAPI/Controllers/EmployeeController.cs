1b19b342309df88b6236661d6afbee649aed45da
Zi Zhang <z785zhan@uwaterloo.ca>
2023-08-31 23:28:18 -0400

    Created API folder, tested get with dummy data

---
+++ EmployeesAPI/EmployeesAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace EmployeesAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : Controller
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<Employee>>> GetEmployee()
+        {
+            return new List<Employee> {
+                new Employee {
+                    Id = 1,
+                    FirstName = "Zijian",
+                    LastName = "Zhang",
+                    Salary = 100000}
+            };
+        }
+    }
+}
---
+++ EmployeesAPI/EmployeesAPI/Employee.cs
@@ -0,0 +1,13 @@
+﻿namespace EmployeesAPI
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; } = string.Empty;
+
+        public string LastName { get; set; } = string.Empty;
+
+        public int Salary { get; set; }
+    }
+}
